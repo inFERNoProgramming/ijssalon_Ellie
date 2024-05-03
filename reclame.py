bb21d98aeb27ff1a6a677d9fb1108e529bcff2c0
inFERNo <168463102+inFERNoProgramming@users.noreply.github.com>
2024-05-03 16:15:09 +0200

    functie inkomste_totaal toegevoegd

--- reclame.py
+++ reclame.py
@@ -2,7 +2,17 @@ def aanbieding_1(smaak, prijs, korting):
     afgetrokken_korting = prijs - prijs * korting
     print(f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {afgetrokken_korting}.")
 
-reclame = aanbieding_1("aarbei", 4, 0.1)
+aanbieding1 = aanbieding_1("aarbei", 4, 0.1)
 
-def inkomsten_totaal(inkomsten):
-    return 
+def inkomsten_totaal(a,b,c,d,e,f,g):
+    inkomsten = a+b+c+d+e+f+g
+    btw = inkomsten * 0.09
+    print(f"Het totaal van alle inkomsten van deze week is {inkomsten} euro, waarover {btw} euro btw betaald dient te worden.")
+
+inkomsten_en_btw = inkomsten_totaal(220,430,125,160,205,90,345)
+
+mijn_lijst = 220,430,125,160,205,90,345
+def hoog_en_laag(mijn_lijst):
+    x = max(mijn_lijst)
+    y = min(mijn_lijst)
+    print(f"max = {x} en min = {y}")
