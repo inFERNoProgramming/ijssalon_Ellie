9a0ab323099082d06eb6853b8726deafd2d7f4fa
inFERNo <168463102+inFERNoProgramming@users.noreply.github.com>
2024-05-03 13:54:55 +0200

    algemene_funties.py aangemaakt

---
+++ algemene_functies.py
@@ -0,0 +1,12 @@
+def mijn_functie_1(a):
+    return a ** 2
+totaal1 = mijn_functie_1(12)
+print(totaal1)
+
+def mijn_functie_2(a,b):
+   print(a+b)
+   print(a-b)
+   print(a*b)
+   print(a//b)
+
+totaal2 = mijn_functie_2(100,20)
