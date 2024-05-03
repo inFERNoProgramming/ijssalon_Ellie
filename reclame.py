62c440186ec70b82e83d7e5ffc3f20d775a3d0f4
inFERNo <168463102+inFERNoProgramming@users.noreply.github.com>
2024-05-03 14:07:43 +0200

    reclame.py aangemaakt

---
+++ reclame.py
@@ -0,0 +1,5 @@
+def aanbieding_1(smaak, prijs, korting):
+    afgetrokken_korting = prijs - prijs * korting
+    print(f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {afgetrokken_korting}.")
+
+reclame = aanbieding_1("aarbei", 4, 0.1)
