f677d3cfc79f9a8ed066a419b8da311b759221dc
inFERNo <168463102+inFERNoProgramming@users.noreply.github.com>
2024-05-03 13:37:26 +0200

    eerste functie toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,18 +1,21 @@
-prijzen = {
-    "aardbei" : 3,
-    "vanille" : 4,
-    "chocolade" : 5
-}
-korting = 0.8
-aanbieding = prijzen["vanille"] * korting
-reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts €{aanbieding}")
-#ik heb geen reclame_tekst2 omdat het mijne er anders uitzag dan in de cursus en ik niet zo goed snapte wat ik er precies mee moest doen.
-reclame_tekst3 = reclame_tekst.upper()
-reclame_tekst4 = reclame_tekst3.split()
-for el in reclame_tekst4:
-    if len(el) >= 5:
-        print(el.upper())
-    else:
-        print(el.lower())
+def print_aanbieding():
+    prijzen = {
+        "aardbei" : 3,
+        "vanille" : 4,
+        "chocolade" : 5
+    }
+    korting = 0.8
+    aanbieding = prijzen["aardbei"] * korting
+    reclame_tekst = (f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts €{aanbieding}")
+    reclame_tekst2 = reclame_tekst[:62]
+    reclame_tekst3 = reclame_tekst2.upper()
+    reclame_tekst4 = reclame_tekst3.split()
+    for el in reclame_tekst4:
+        if len(el) >= 5:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+print_aanbieding()
 
 
