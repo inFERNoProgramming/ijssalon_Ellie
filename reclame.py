c113d33176e10337ec478b88be97c0d15598f2cd
inFERNo <168463102+inFERNoProgramming@users.noreply.github.com>
2024-05-03 15:38:51 +0200

    inkomsten_totaal toegevoegd

--- reclame.py
+++ reclame.py
@@ -3,3 +3,6 @@ def aanbieding_1(smaak, prijs, korting):
     print(f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs} euro voor {afgetrokken_korting}.")
 
 reclame = aanbieding_1("aarbei", 4, 0.1)
+
+def inkomsten_totaal(inkomsten):
+    return 
